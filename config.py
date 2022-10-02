6bc9e9613ea04d5a77f5c559c4a8481d288e8646
alexlu07 <alexlu.manhasset@gmail.com>
2022-10-02 12:54:40 -0400

    create Buffer class

--- config.py
+++ config.py
@@ -5,7 +5,11 @@ class Config:
         self.env = env
         self.obs_shape = env.observation_space.shape
         self.act_shape = env.action_space.shape
+        self.n_envs = 4
+        
         self.num_options = 5
+
+        self.batch_size = 32
 
         self.lr = 1e-3
         self.temperature = 1.0
